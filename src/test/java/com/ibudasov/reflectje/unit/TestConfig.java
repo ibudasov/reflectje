4312ac4fd8482c9e42f2a4917575eea6c3910e5b
Igor Budasov <igor.budasov@gmail.com>
2019-02-13 21:15:14 +0100

    removing non existent service from test config #2

--- src/test/java/com/ibudasov/reflectje/unit/TestConfig.java
+++ src/test/java/com/ibudasov/reflectje/unit/TestConfig.java
@@ -1,15 +1,8 @@
 package com.ibudasov.reflectje.unit;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.ibudasov.reflectje.service.ReflectjeService;
 
 @Configuration
 public class TestConfig {
 
-//    @Bean
-//    public ReflectjeService getReflectjeService() {
-//        return new ReflectjeService();
-//    }
 }
