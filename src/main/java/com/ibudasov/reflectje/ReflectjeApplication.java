e73d2524a4b95a08ef1757d128baf51010c8585e
Igor Budasov <igor.budasov@gmail.com>
2019-02-13 21:18:48 +0100

    missing main application file

---
+++ src/main/java/com/ibudasov/reflectje/ReflectjeApplication.java
@@ -0,0 +1,13 @@
+package com.ibudasov.reflectje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectjeApplication.class, args);
+	}
+}
+
