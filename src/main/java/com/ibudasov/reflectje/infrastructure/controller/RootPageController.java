fe52dff259c54662f4790bab47b1006ba90fdd47
Igor Budasov <igor.budasov@gmail.com>
2019-02-17 09:02:09 +0100

    integrate swagger -- API docs are available at localhost:8080/v2/api-docs, and also add excludes for mutation check and coverage check

--- src/main/java/com/ibudasov/reflectje/infrastructure/controller/RootPageController.java
+++ src/main/java/com/ibudasov/reflectje/infrastructure/controller/RootPageController.java
@@ -3,12 +3,13 @@ package com.ibudasov.reflectje.infrastructure.controller;
 import com.ibudasov.reflectje.application.response.RootPageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @RestController
-@RequestMapping(path = "/", produces = APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(path = "/", produces = APPLICATION_JSON_UTF8_VALUE, method = RequestMethod.GET)
 public class RootPageController {
 
     @GetMapping(path = "/")
