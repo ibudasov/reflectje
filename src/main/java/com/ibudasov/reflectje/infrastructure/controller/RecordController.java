cd39b666c3f79e7b44e7dd4a339aeb00c017bed5
Igor Budasov <igor.budasov@gmail.com>
2019-10-11 20:54:24 +0200

    specifying response status explicitly

--- src/main/java/com/ibudasov/reflectje/infrastructure/controller/RecordController.java
+++ src/main/java/com/ibudasov/reflectje/infrastructure/controller/RecordController.java
@@ -2,8 +2,10 @@ package com.ibudasov.reflectje.infrastructure.controller;
 
 import com.ibudasov.reflectje.application.response.CreateRecordResponse;
 import com.ibudasov.reflectje.infrastructure.Uuid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -13,6 +15,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 public class RecordController {
 
     @PostMapping(path = "/")
+    @ResponseStatus(value = HttpStatus.OK)
     public CreateRecordResponse createRecord() {
 
         // take json string with an array
