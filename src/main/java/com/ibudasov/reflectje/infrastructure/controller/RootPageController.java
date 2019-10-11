3c809a3f7e3df26950baee269751eb16c8c3c4ae
Igor Budasov <igor.budasov@gmail.com>
2019-10-11 19:49:07 +0200

    removing comments, because I moved them to the new controller, it is coming

--- src/main/java/com/ibudasov/reflectje/infrastructure/controller/RootPageController.java
+++ src/main/java/com/ibudasov/reflectje/infrastructure/controller/RootPageController.java
@@ -14,15 +14,6 @@ public class RootPageController {
 
     @GetMapping(path = "/")
     public RootPageResponse rootPage() {
-
-        // take json string with an array
-        // parse it as a key-val
-        // Uppercase key
-        // try to instantiate Experience related to the key
-        // if doesn't work - throw
-        // if works -- add to accumulator hashmap
-        // return it
-
         RootPageResponse rootPageResponse = new RootPageResponse();
 
         return rootPageResponse.setGreeting("Hoi alemaal!");
