591ce7a83dc54ac2e7c6188c20026a7c2500f69a
Igor Budasov <igor.budasov@gmail.com>
2019-10-11 21:19:32 +0200

    removing dead code

--- src/main/java/com/ibudasov/reflectje/application/request/CreateRecordRequest.java
+++ src/main/java/com/ibudasov/reflectje/application/request/CreateRecordRequest.java
@@ -6,9 +6,4 @@ public class CreateRecordRequest {
     public String getTestInput() {
         return testInput;
     }
-
-    public CreateRecordRequest setTestInput(String testInput) {
-        this.testInput = testInput;
-        return this;
-    }
 }
