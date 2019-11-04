5c6ef3d5269902d2e1babcdbafe097ded6259e7c
Igor Budasov <igor.budasov@gmail.com>
2019-11-04 19:10:20 +0100

    some comments

--- src/main/java/com/ibudasov/reflectje/domain/Experience.java
+++ src/main/java/com/ibudasov/reflectje/domain/Experience.java
@@ -1,6 +1,14 @@
 package com.ibudasov.reflectje.domain;
 
-// todo: maybe I should make it an abstract class? But I don't see any advantage so far
+/**
+ * The central piece of the application.
+ * Experience might be: a dream, a fact, a feeling, an emotion.
+ * Basically, anything what can influence the patients emotional consciousness.
+ */
 public interface Experience {
+
+    // todo: maybe I should make it an abstract class? But I don't see any advantage so far
+    // todo: what about moving getters ans setters here, at least?
+
     String getDescription();
 }
