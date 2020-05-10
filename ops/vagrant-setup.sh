daa42be4c2b28b6de4ac9c3d0368ce47fdca6896
Igor Budasov <igor.budasov@gmail.com>
2020-05-10 17:30:25 +0200

    Moving application-related stuff to a separate folder

--- ops/vagrant-setup.sh
+++ ops/vagrant-setup.sh
@@ -15,3 +15,20 @@ sudo apk add lynx
 
 ansible-playbook reflectje/ops/vagrant-setup.yml
 ansible-playbook reflectje/ops/vagrant-setup-kubernetes.yml
+
+sudo apk --update --no-cache add docker sudo bash
+ 
+sudo apk upgrade --update 
+
+sudo apk add --no-cache --virtual=.build-dependencies ca-certificates python2 wget go make 
+sudo apk add autoconf findutils make pkgconf libtool g++ automake linux-headers git 
+
+# sudo wget "https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl" -O "/usr/local/bin/kubectl" && \
+# sudo mkdir -p /usr/bin/src/k8s.io && sudo cd /usr/bin/src/k8s.io sudo && sudo chmod +x /usr/local/bin/kubectl && \
+
+sudo git clone https://github.com/kubernetes/minikube && cd minikube && \
+
+sudo make && sudo mv ./out/minikube /usr/local/bin/minikube && sudo rm -rf /usr/bin/src/k8s.io && sudo rm -rf /tmp/* 
+
+sudo minikube start --vm-driver none --kubernetes-version v1.7.5 -v 7 --memory 1024 --disk-size 4g && \
+sudo apk del .build-dependencies
